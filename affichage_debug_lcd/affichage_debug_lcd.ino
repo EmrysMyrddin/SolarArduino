cff0cbe658e9657a230743804cac5b81bd722fc5
Maël N <contact@maeln.com>
2015-10-12 16:58:24 +0200

    Ajout du débug LCD

--- affichage_debug_lcd/affichage_debug_lcd.ino
+++ affichage_debug_lcd/affichage_debug_lcd.ino
@@ -1,4 +1,5 @@
 #include <ST7565.h>
+#include <Servo.h>
 
 int ledPin =  13;    // LED connected to digital pin 13
 
@@ -21,7 +22,13 @@ unsigned int val_res3 = 500;
 unsigned int val_res4 = 250;
 
 int rot1 = 20;
-int rot2 = 90;
+int rot2 = -1;
+
+Servo servo1;
+Servo servo2;
+
+int const servoPin1 = 5;
+int const servoPin2 = 6;
 
 void setup()   { 
 
@@ -33,6 +40,9 @@ void setup()   {
   glcd.begin(0x00);
   glcd.st7565_command(CMD_SET_RESISTOR_RATIO | 0x05);
   glcd.clear();
+  
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
 }
 
 void int_to_char(char* in, int val, char* out)
@@ -46,10 +56,25 @@ void int_to_char(char* in, int val, char* out)
   res.toCharArray(out,10);
 }
 
+bool needToMove = false;
+int posServo = 10;
+
 void loop()
 {
   val_res1 = analogRead(1);
   val_res2 = analogRead(2);
+  
+  needToMove = abs(val_res1 - val_res2) > (needToMove ? 20 : 40);
+  
+  int delta = (val_res1 - val_res2);
+  int div = abs(delta);
+  
+  int pas = needToMove ?  delta / div : 0;
+  posServo += pas;
+  posServo = posServo >= 10 ? posServo <= 170 ? posServo : 170 : 10;
+  
+  servo1.write(posServo);
+  servo2.write(posServo);
   
   char tmp[10];
   glcd.clear();
@@ -60,19 +85,18 @@ void loop()
   int_to_char("RES2:", val_res2, tmp);
   glcd.drawstring(0, 1, tmp);
   
-  int_to_char("RES3:", val_res3, tmp);
+  int_to_char("RES3:", pas, tmp);
   glcd.drawstring(0, 2, tmp);
   
   int_to_char("RES4:", val_res4, tmp);
   glcd.drawstring(0, 3, tmp);
   
-  int_to_char("ROT1:", rot1, tmp);
+  int_to_char("ROT1:", posServo, tmp);
   glcd.drawstring(0, 5, tmp);
   
   int_to_char("ROT2:", rot2, tmp);
   glcd.drawstring(0, 6, tmp);
   
-  
   glcd.display();
-  delay(100);
+  delay(10);
 }
